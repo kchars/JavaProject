eb40e19b9d8366e4299063c15b914f7ce0ec30ec
admin <admin@YD01-11>
2021-02-18 15:20:54 +0900

    koreantest delite

--- javaProject/src/common/KoreanTest.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class KoreanTest {
-	public static void main(String[] args) {
-		
-		System.out.println("한글테스트입니다.");
-	}
-
-}
--- javaProject/src/common/KoreanTest4.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class KoreanTest4 {
-	public static void main(String[] args) {
-		
-		System.out.println("한글테스트4.");
-	}
-
-}
