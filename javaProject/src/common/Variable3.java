f7ca7a8b7b0acfee1671a36d066e5d72f2720b13
admin <admin@YD01-11>
2021-02-18 15:47:53 +0900

    Test

---
+++ javaProject/src/common/Variable3.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Variable3 {
+	public static void main(String[] args) {
+		int a, b, result;
+		a = 100;
+		b = 200;
+		result = a + b;
+		System.out.println("������� " + result + " �Դϴ�.");
+	}
+
+}
