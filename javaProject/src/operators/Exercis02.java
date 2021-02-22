361a5e9b52949344ed5416012f1b6943c79a51c9
kchar <kchar@DESKTOP-QTM3KPM>
2021-02-22 23:03:24 +0900

    02.22 homework

---
+++ javaProject/src/operators/Exercis02.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class Exercis02 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
