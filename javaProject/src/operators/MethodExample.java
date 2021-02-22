b1f2d8cd97d93fe70cc090a278652e6e33ec41c1
admin <admin@YD01-11>
2021-02-22 11:36:45 +0900

    02.22

---
+++ javaProject/src/operators/MethodExample.java
@@ -0,0 +1,40 @@
+package operators;
+
+public class MethodExample {
+	public static void main(String[] args) {
+		int result = sum(10, 20);
+		// result = 30,20의 차이를 계산하는 minus()
+		int result1 = minus(30, 20);
+		// result = 15,13의 차이를 계산하는 multi()
+		int result2 = multi(15, 13);
+		// result = 30,12의 차이를 계산하는 divide()
+		double result3 = divide(30, 12);
+
+		System.out.println("결과 : " + result);
+		System.out.println("결과 : " + result1);
+		System.out.println("결과 : " + result2);
+		System.out.println("결과 : " + result3);
+
+	}
+
+	public static int sum(int a, int b) {
+		int result = a + b;
+		return result;
+	}
+
+	public static int minus(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+
+	public static int multi(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+
+	public static double divide(double a, double b) {
+		double result = a / b;
+		return result;
+	}
+
+}
